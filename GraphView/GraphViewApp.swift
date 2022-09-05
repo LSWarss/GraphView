e0438d44aee3ccafa46ba1578fbc60724ad0e208
LSWarss <ls.warss98@gmail.com>
2022-09-05 19:59:57 +0200

    Initial Commit

---
+++ GraphView/GraphViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphViewApp.swift
+//  GraphView
+//
+//  Created by Łukasz Stachnik on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
